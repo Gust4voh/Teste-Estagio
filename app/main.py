21726a30361dd1da00295e2989970646b108170e
Gustavo <gustavosalesdeoliveira2004@gmail.com>
2026-02-02 20:28:37 -0300

    main

--- app/main.py
+++ app/main.py
@@ -1,31 +1,39 @@
+from config.vars import PASTA_ZIPS, PASTA_EXTRAIDOS
+
 from receber_dados.listar_anos import listar_anos_disponiveis
 from receber_dados.listar_trimestres import listar_trimestres
 from receber_dados.listar_zips import listar_zips_trimestre
 from receber_dados.baixar_zips import baixar_zips
-from config.vars import PASTA_ZIPS
+from receber_dados.extrair_zips import extrair_zips
 
-def obter_ultimos_trimestres(quantidade=3):
-    resultados = []
 
+def main():
     anos = listar_anos_disponiveis()
 
-    for ano in anos:
-        trimestres = listar_trimestres(ano)
+    if not anos:
+        return
 
-        for trimestre in trimestres:
-            resultados.append((ano, trimestre))
+    ano_mais_recente = anos[0]
 
-            if len(resultados) == quantidade:
-                return resultados
-            
-    return resultados
+    trimestres = listar_trimestres(ano_mais_recente)
+
+    if not trimestres:
+        return
+
+    ultimos_trimestres = trimestres[:3]
+
+    urls_zips = []
+    for trimestre in ultimos_trimestres:
+        urls = listar_zips_trimestre(ano_mais_recente, trimestre)
+        urls_zips.extend(urls)
+
+    if not urls_zips:
+        return
+
+    arquivos_zip = baixar_zips(urls_zips, PASTA_ZIPS)
+
+    extrair_zips(arquivos_zip, PASTA_EXTRAIDOS)
+
 
 if __name__ == "__main__":
-
-    urls = listar_zips_trimestre(2025, "3T")
-
-    baixar_zips(
-        urls,
-        PASTA_ZIPS
-    )
-
+    main()
